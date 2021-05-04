cbbdb60c6f29830fd8666d25653c8d46f6876586
Thiago Costa <thiagohrcosta86@gmail.com>
2021-05-04 19:36:40 -0300

    Adds update action on controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -29,7 +29,12 @@ class MoviesController < ApplicationController
   end
 
   def update
-
+    @movie = Movie.find(params[:id])
+    if @movie.update(movie_params)
+      redirect_to movie_path(@movie)
+    else
+      render :new
+    end
   end
 
   private
