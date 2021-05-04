048283f6ef4aac16a3cc2ee3e02abe7989513ca1
Thiago Costa <thiagohrcosta86@gmail.com>
2021-05-04 12:43:36 -0300

    Adds search with OR

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,10 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.all
+    if params[:query]
+      query = "title @@ :query OR synopsis @@ :query"
+      @movies = Movie.where(query, query: "%#{params[:query]}%")
+    else
+      @movies = Movie.all
+    end
   end
 end
