93f5b2c4391149923de2fb64f2e2a1bc676dd4d9
Thiago Costa <thiagohrcosta86@gmail.com>
2021-05-04 13:47:52 -0300

    Adds weight to PgSearch

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,9 +1,12 @@
 class Movie < ApplicationRecord
   belongs_to :director
-  include PgSearch: :Model
 
+  include PgSearch::Model
   pg_search_scope :search_by_title_and_synopsis,
-    against: [:title, :synopsis],
+    against: [ title: 'A', synopsis: 'C' ],
+    associated_against: {
+      director: [ :first_name, :last_name ]
+    },
     using: {
       tsearch: { prefix: true }
     }
